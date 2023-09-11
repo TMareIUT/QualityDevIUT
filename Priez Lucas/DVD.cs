ff1b41d54964b8c0b3782acce7976e065f7057b0
Natsury <66617407+Natsury@users.noreply.github.com>
2023-09-11 18:55:37 +0200

    Add files via upload

---
+++ Priez Lucas/DVD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premiercours
+{
+    public class DVD : Media
+    {
+        private string duree;
+    
+        public DVD(string titre, int noReference, int nbExemplairesDispo, string duree) : base(titre, noReference, nbExemplairesDispo)
+        {
+            this.duree = duree;
+        }
+    
+        public override void AfficheInfo()
+        {
+            base.AfficheInfo();
+            Console.WriteLine("Auteur : " + this.duree);
+        }
+    }
+}
---
+++ Priez Lucas/Media.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premiercours
+{
+    public class Media
+    {
+        private string titre;
+
+        private int noReference;
+
+        private int nbExemplairesDispo;
+
+        /// <summary>
+        /// Constructeur de la classe Média
+        /// </summary>
+        /// <param name="titre">Le titre du média</param>
+        /// <param name="noReference">Le numéro de référence</param>
+        /// <param name="nbExemplairesDispo">Le nombre d'exemplaire disponnible</param>
+        public Media(string titre, int noReference, int nbExemplairesDispo)
+        {
+            this.titre = titre;
+            this.noReference = noReference;
+            this.nbExemplairesDispo = nbExemplairesDispo;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string getTitre()
+        {
+            return this.titre;
+        }
+
+        public int getNoReference()
+        {
+            return this.noReference;
+        }
+
+        public int getNbExemplaire()
+        {
+            return this.nbExemplairesDispo;
+        }
+
+        public virtual void AfficheInfo()
+        {
+            Console.WriteLine("Titre : " + this.getTitre() +
+                                "\nNuméro de références : " + this.getNoReference() +
+                                "\nNombres d'exemplaires disponibles : " + this.getNbExemplaire());
+        }
+
+
+    }
+}
