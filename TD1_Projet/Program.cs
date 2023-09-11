bc50d07c0e5cfc99bf90e6ac757ec66b0a802c03
Martin <martin.gotz@etud.u-picardie.fr>
2023-09-11 18:28:28 +0200

    Initial Commit

---
+++ TD1_Projet/Program.cs
@@ -0,0 +1,22 @@
+﻿//MAIN
+
+using TD1_Projet;
+
+Library bibliotheque = new Library();
+
+Livre livre1 = new Livre("Le Seigneur des Anneaux", 101, 5, "J.R.R. Tolkien");
+Livre livre2 = new Livre("La Horde du Contrevent", 102, 4, "Alain Damasio");
+Livre livre3 = new Livre("Le Cycle de Fondation T1 : Fondation", 102, 6, "Isaac Asimov");
+
+bibliotheque.AjouterMedia(livre1);
+
+// Ajouter un livre à la bibliothèque
+bibliotheque += livre2;
+bibliotheque += livre3;
+
+// Retirer un livre de la bibliothèque
+bibliotheque -= livre2;
+
+
+bibliotheque.AfficherTousLesMedias();
+bibliotheque.AfficherStatistiques();
